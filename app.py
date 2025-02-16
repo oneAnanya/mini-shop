59d3c91c7d6808237e603c38285a16e4626a7271
OneAnanya <ananyamohapatra61@gmail.com>
2025-02-16 21:54:15 +0530

    db int

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///products.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+db=SQLAlchemy(app)
+
+class Product(db.Model):  
+    id=db.Column(db.Integer,primary_key=True)
+    p_name=db.Column(db.String(20),nullable=False)
+    p_price=db.Column(db.Integer,nullable=False)
+    p_quantity=db.Column(db.Integer,nullable=False)
+
+class Contact(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20),nullable=False)
+    email=db.Column(db.String(30),nullable=False)
+    message=db.Column(db.String(100),nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
@@ -20,5 +40,4 @@ def contactus():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
-
+    app.run(debug=True) 
